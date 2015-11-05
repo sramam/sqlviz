72535054b459045e3a73d7ad80d4d42b862f0225
Shishir Ramam <sramam@gwadmins-mbp-78.attlocal.net>
2015-11-05 05:00:11 -0800

    adding output control to the cli

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ var program = require('commander'),
     Promise = require('bluebird'),
     inquirer = require('inquirer'),
     prompt = Promise.promisify(inquirer.prompt),
+    pjson = require('./package.json'),
     sqlviz = {
         mysql: require('./lib/mysql.js')
     };
 
 program
-  .version('0.0.1')
-  .option('-h, --host [host]', 'database host', null)
-  .option('-u, --user [user]', 'database user', null)
-  .option('-p, --password [pasword]', 'password for username', null)
+  .version(pjson.version)
   .option('-d, --database [database]', 'database to extract schema from', null)
+  .option('-o, --output [filename]', 'filename to write the database to', './dbviz.png')
+  .option('-h, --host [host]', 'database host', 'localhost')
+  .option('-u, --user [user]', 'database user', 'root')
+  .option('-p, --password [password]', 'password for username', null)
   .parse(process.argv);
 
+if (program.database === null) {
+    console.error("database is a required parameter");
+    process.exit(1);
+}
+/*
 if (program.host === null) {
     console.error("Host is a required parameter");
     process.exit(1);
@@ -27,10 +34,7 @@ if (program.user === null) {
 if (program.password === null) {
     console.error("password is a required parameter");
     process.exit(1);
-}
-if (program.database === null) {
-    console.error("database is a required parameter");
-    process.exit(1);
-}
+    }
+    */
 
-sqlviz.mysql.get_schema(program.database, program.host, program.user, program.password);
+sqlviz.mysql.get_schema(program.database, program.output, program.host, program.user, program.password);
